f6c2d725f0f0a9a33a4f473b66e1eba85f3c7b94
HoangHuy <jayker03212k5@gmail.com>
2026-04-15 12:10:13 +0700

    guide update

--- Controllers/AboutController.cs
+++ Controllers/AboutController.cs
@@ -1,11 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Delivery_System.Models;
 
 namespace Delivery_System.Controllers
 {
     public class AboutController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public AboutController(AppDbContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var stationList = await _context.TblStations
+                .AsNoTracking()
+                .Where(s => s.IsActive == true)
+                .OrderBy(s => s.StationId)
+                .ToListAsync();
+
+            ViewBag.StationList = stationList;
             return View();
         }
     }
